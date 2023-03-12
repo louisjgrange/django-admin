f18dd19abee648f1e9b4dcffc48a51555a2eaca9
Louis Grange <70532216+louisjgrange@users.noreply.github.com>
2023-03-12 14:09:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(
+   name='django-admin-magnify',
+   version='0.0',
+   description='A useful module',
+   author='Louis Grange',
+   packages=['django-admin-magnify'],
+   install_requires=['django>=4.0'],
+)
